a605296633bca73cb35a8479a58369748beaad97
vtrtga <vitor.ypvalim@gmail.com>
2023-09-23 08:31:52 -0300

    style: header

---
+++ src/components/Header/headerClasses.ts
@@ -0,0 +1,11 @@
+import { HeaderStyles } from "./HeaderTypes";
+
+
+const styles: HeaderStyles = {
+    parentDiv: "layout-header w-full",
+    imageHeight: "w-full",
+    imageWidth: "w-full",
+    backgroundColor: "bg-indigo"
+}
+
+export default styles;
