c71fe6ba485dd39d542814e430d408340d5779e0
vtrtga <vitor.ypvalim@gmail.com>
2023-09-23 00:48:33 -0300

    feat: layout

---
+++ src/pages/app.tsx
@@ -0,0 +1,12 @@
+import Header from '@/components/Header/Header';
+import React from 'react';
+
+function TansegLayout() {
+    return(
+        <div className="main-layout">
+            <Header />
+        </div>
+    )
+}
+
+export default TansegLayout;
