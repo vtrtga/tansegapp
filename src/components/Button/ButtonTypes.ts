080ce47a8e80200b655ad34521ac77ce13c8d826
vtrtga <vitor.ypvalim@gmail.com>
2023-09-23 08:34:51 -0300

    style: button styles

--- src/components/Button/ButtonTypes.ts
+++ src/components/Button/ButtonTypes.ts
@@ -1,6 +1,11 @@
 import { SyntheticEvent } from "react";
 
+export interface Styles {
+    backgroundColor: string;
+}
+
 export interface ButtonProps {
     className: string;
     onClick: (e: SyntheticEvent) => void;
+    styles: Styles;
 }
