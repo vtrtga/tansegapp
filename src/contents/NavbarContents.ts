d76dcb664b0d34175182cad3e48135648d2a2971
vtrtga <vitor.ypvalim@gmail.com>
2023-09-27 05:37:10 -0300

    feat: contents object

---
+++ src/contents/NavbarContents.ts
@@ -0,0 +1,13 @@
+import { Item } from "@/components/Navbar/NavbarTypes"
+
+export const navItens: Item[] = [
+    {
+        title: "Início"
+    },
+    {
+        title: "Produtos"
+    },
+    {
+        title: "Contato"
+    }
+]
