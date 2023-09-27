6d76dbdf09b7db12130041dd60f9c971f11cfcc9
vtrtga <vitor.ypvalim@gmail.com>
2023-09-27 05:37:30 -0300

    feat: navbar default classes

---
+++ src/components/Navbar/NavbarClasses.ts
@@ -0,0 +1,11 @@
+import { NavbarStyles } from "./NavbarTypes";
+
+
+const styles: NavbarStyles = {
+    parentDiv: "layout-Navbar w-full",
+    imageHeight: "w-full",
+    imageWidth: "w-full",
+    backgroundColor: "bg-indigo"
+}
+
+export default styles;
