23a1d1ce6aa6c3536653dd777e7764f07d6fb152
vtrtga <vitor.ypvalim@gmail.com>
2023-10-21 10:51:41 -0300

    feat: implement logo

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,11 +2,16 @@ import "./Navbar.css";
 import classNames from 'classnames';
 import { NavbarProps } from './NavbarTypes';
 import HamburguerMenu from "./HamburguerMenu";
+import Logo from "@/images/logo.png";
+import Image from "next/image";
 
 function Navbar({ className }: NavbarProps) {
     return(
         <nav
         className={classNames(className, "navbar")} >
+            <p className="logo-container">
+            <Image src={Logo} className="logo" alt="logo"/>
+            </p>
             <HamburguerMenu />
         </nav>
     )
